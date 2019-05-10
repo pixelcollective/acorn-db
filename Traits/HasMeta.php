727ab939062f1a743768ce9d52f283c8e213ea69
Kelly Mears <developers@tinypixel.dev>
2019-05-10 11:24:07 -0700

    :tada: add helpers & stuff

---
+++ Traits/HasMeta.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models\Traits;
+
+use \App\Models\Helpers\WordPress;
+
+trait HasMeta
+{
+    public function getMeta($meta_key = false)
+    {
+        $meta_value = '';
+
+        if ($meta_key) {
+            $meta_value = $this->meta()->where('meta_key', $meta_key)->pluck('meta_value')->first();
+
+            if (WordPress::isSerialized($meta_value)) {
+                $meta_value = unserialize($meta_value);
+            }
+        }
+
+        return $meta_value;
+    }
+
+    public function setMeta($key, $value)
+    {
+        $value = is_array($value) ? serialize($value) : $value;
+        $meta  = $this->meta()->firstOrCreate(['meta_key' => $key]);
+        $meta  = $this->meta()->where(['meta_key' => $key])->update(['meta_value' => $value]);
+
+        return $this;
+    }
+
+    /**
+     * Deletes all meta for this object with given key
+     */
+    public function deleteMeta($meta_key = false)
+    {
+        if ($meta_key) {
+            $this->meta()->where('meta_key', $meta_key)->delete();
+        }
+    }
+}
---
+++ Traits/HasRoles.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models\Traits;
+
+trait HasRoles
+{
+    public function hasRole($role = '')
+    {
+        return in_array($role, $this->capabilities);
+    }
+
+    public function hasAnyRoles($roles = [])
+    {
+        if (!empty($roles)) {
+            foreach ($roles as $role) {
+                if ($this->hasRole($role)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public function getCapabilitiesAttribute()
+    {
+        return array_keys($this->getMeta('wp_capabilities'));
+    }
+
+    public function getIsAdminAttribute()
+    {
+        return $this->hasRole('administrator');
+    }
+}
